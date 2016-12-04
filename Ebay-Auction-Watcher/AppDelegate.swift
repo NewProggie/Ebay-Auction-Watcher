44724196d7121a2524179a4d17a6a2e56ff2aec0
Kai Wolf <kai.wolf@gmail.com>
2016-12-04 10:08:50 +0100

    Commit swift boilerplate code

--- Ebay-Auction-Watcher/AppDelegate.swift
+++ Ebay-Auction-Watcher/AppDelegate.swift
@@ -1,27 +1,48 @@
-//
-//  AppDelegate.swift
-//  Ebay-Auction-Watcher
-//
-//  Created by Kai Wolf on 04.12.16.
-//  Copyright © 2016 Kai Wolf. All rights reserved.
-//
+// Copyright (c) 2016, Kai Wolf. All rights reserved.
+// Use of this source code is governed by a MIT-style license that can be
+// found in the LICENSE file in the top directory.
 
 import Cocoa
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate : NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
 
+    let statusItem = NSStatusBar.system().statusItem(withLength: -2);
+    let popover = NSPopover()
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        if let button = statusItem.button {
+            button.image = NSImage(named: "StatusBarButtonImage")
+            button.action = #selector(AppDelegate.togglePopover)
+        }
+
+        popover.contentViewController = AuctionViewController(nibName: "AuctionViewController", bundle: nil)
+
     }
+
+
+    func showPopover(sender: AnyObject?) {
+        if let button = statusItem.button {
+            popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+        }
+    }
+
+    func closePopover(sender: AnyObject?) {
+        popover.performClose(sender)
+    }
+
+    func togglePopover(sender: AnyObject?) {
+        if popover.isShown {
+            closePopover(sender: sender)
+        } else {
+            showPopover(sender: sender)
+        }
+    }
+
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
 }
-
---
+++ Ebay-Auction-Watcher/AuctionViewController.swift
@@ -0,0 +1,13 @@
+// Copyright (c) 2016, Kai Wolf. All rights reserved.
+// Use of this source code is governed by a MIT-style license that can be
+// found in the LICENSE file in the top directory.
+import Cocoa
+
+class AuctionViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
