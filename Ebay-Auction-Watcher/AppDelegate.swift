d888cc1d974674ec27ab7ff76025fe5c71d7af1b
Kai Wolf <kai.wolf@gmail.com>
2016-12-04 10:01:14 +0100

    Initial Commit

---
+++ Ebay-Auction-Watcher/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Ebay-Auction-Watcher
+//
+//  Created by Kai Wolf on 04.12.16.
+//  Copyright © 2016 Kai Wolf. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
